08619afbb6ae0e61c1feef724f8da65219319b7c
lestox <78364535+lestox@users.noreply.github.com>
2022-04-06 12:19:27 +0200

    ajout bootstrap

--- app/post_react/src/App.tsx
+++ app/post_react/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App () {
+    return (
+        <section className="vh-100" style={{backgroundColor: '#eee'}}>
+            <div className="container py-5 h-100">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col col-lg-9 col-xl-7">
+                        <div className="card rounded-3">
+                            <div className="card-body p-4">
+
+                                <h4 className="text-center my-3 pb-3">To Do App</h4>
+
+                                <Form setTodos={setTodos} todos={todos}/>
+
+                                {todos.map((todo) => (
+                                    (<Todo title={todo.title} text={todo.text} key={todo.id}/>)
+                                ))}
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        )
 }
-
-export default App;
