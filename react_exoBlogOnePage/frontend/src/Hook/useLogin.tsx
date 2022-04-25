4a2e72c2ce69dc19aabd3d66f57a8d34afc9cd3a
lestox <78364535+lestox@users.noreply.github.com>
2022-04-25 16:56:06 +0200

    remplacmeent de fetch par axios

--- react_exoBlogOnePage/frontend/src/Hook/useLogin.tsx
+++ react_exoBlogOnePage/frontend/src/Hook/useLogin.tsx
@@ -1,15 +1,17 @@
+import axios from 'axios';
+
 import {LoginResponseInterface} from "../Interface/ResponsesInterfaces";
 
 export default function useLogin() {
     return (username: string, password: string): Promise<LoginResponseInterface> => {
-        return fetch('http://localhost:2345/login.php', {
-            method: 'GET',
-            mode: 'cors',
-            credentials: "include",
+        return axios({
+            url : 'http://localhost:2345/login.php',
+            method: 'get',
+            withCredentials: true,
             headers: {
                 Authorization: `Basic ${btoa(username + ':' + password)}`
-            }
+            },
         })
-            .then(res => res.json())
+            .then(res => res.data)
     }
 }
--- react_exoBlogOnePage/frontend/src/Hook/usePostBlog.tsx
+++ react_exoBlogOnePage/frontend/src/Hook/usePostBlog.tsx
@@ -1,22 +1,21 @@
 import {LocalBlogPost} from "../Interface/LocalBlogPost";
+import axios from "axios";
+
 
 export default function usePostBlog() {
     return (token: string, blog: LocalBlogPost) => {
-        return fetch('http://localhost:2345/post-blog.php', {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'include',
-            // Ici aussi je pourrais me passer de passer le token en
-            // Bearer et juste le passer par cookie (qui est envoyé
-            // automatiquement et le récupérer en PHP !
+        return axios({
+            url : 'http://localhost:2345/post-blog.php',
+            method: 'post',
+            withCredentials: true,
             headers: {
                 Authorization: `Bearer ${token}`
             },
-            body: new URLSearchParams({
+            data: new URLSearchParams({
                 title: blog.title,
                 content: blog.content
             })
         })
-            .then(res => res.json())
+            .then(res => res.data)
     }
 }
